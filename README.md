74da87f09fd3a2086cee10f69a7f564e8c1a416b
samverme <sam.vermeulen@ugent.be>
2020-01-18 09:03:04 +0100

    baseline

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fastapi-exploration
+
+This is a project to experiment with FastAPI.
+
+Starting the webserver is currently done by running `uvicorn api:app --reload` in src/softwarepackages.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='softwarepackaging',
+      version='1.0',
+      description='Exploring the capabilities of FastAPI',
+      author='Sam Vermeulen',
+      author_email='sam.verm@yahoo.com',
+      packages=find_packages(where='src'),
+      )
